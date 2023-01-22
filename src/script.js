ad7e4d76ddf351ff384e26f7d7b6c4e311fb7d1d
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-01-22 16:58:20 +0100

    follow pointer add

--- src/script.js
+++ src/script.js
@@ -2,9 +2,53 @@ import "./style.css";
 import * as THREE from "three";
 import gsap from "gsap";
 
+
+(function() {
+  
+
+  const follower = document.querySelector('#follower');
+  
+  let posX = 0;
+  let posY = 0;
+  let mouseX = 0;
+  let mouseY = 0;
+  
+  const ease = 0.1;
+  
+  function easeTo() {    
+    const followerBounds = follower.getBoundingClientRect();
+    
+    const dX = mouseX - (followerBounds.left + 10);
+    const dY = mouseY - (followerBounds.top + 10);
+    
+    posX += dX * ease;
+    posY += dY * ease;
+  }
+  
+  function update() {
+    easeTo();
+    follower.style.transform = `translate3d(${posX}px, ${posY}px, 0)`;  
+    requestAnimationFrame(update);
+  }
+  
+  function setCoords(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  }
+  
+	document.onmousemove = setCoords;
+  update();
+})();
+
 var swiper = new Swiper(".mySwiper", {
-  effect: "cards",
-  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 });
 /**
  * Base
