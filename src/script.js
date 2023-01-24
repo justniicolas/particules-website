e5c2f512d6d8a44ccf231ca29750e0780579d010
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-01-24 20:12:05 +0100

    webpack --> vite

--- src/script.js
+++ src/script.js
@@ -1,43 +1,41 @@
-import "./style.css";
-import * as THREE from "three";
-import gsap from "gsap";
+import * as THREE from 'three'
 
 
 /**
  * Swiper
  */
 var swiper = new Swiper(".mySwiper", {
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-});
-
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+  });
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 /**
  * Objects
  */
-//Texture
-const textureLoader = new THREE.TextureLoader();
-const gradientTexture = textureLoader.load("textures/gradients/s.jpg");
-gradientTexture.magFilter = THREE.NearestFilter;
+// Texture
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
 
 // Material
 const material = new THREE.MeshToonMaterial({
-  color: "#ffeded",
-  gradientMap: gradientTexture,
-});
+    color: '#ffeded',
+    gradientMap: gradientTexture
+})
 
 // Objects
+
 const objectsDistance = 4;
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
 const mesh2 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
@@ -59,157 +57,138 @@ mesh4.position.y = -objectsDistance * 3;
 mesh5.position.y = -objectsDistance * 4;
 mesh6.position.y = -objectsDistance * 5;
 
-const sectionMeshes = [mesh1, mesh2, mesh3, mesh4, mesh5, mesh6];
+const sectionMeshes = [mesh1, mesh2, mesh3, mesh4,mesh5,mesh6];
+
+
+
+
 
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
-directionalLight.position.set(1, 1, 0);
-scene.add(directionalLight);
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1, 1, 0)
+scene.add(directionalLight)
 
 /**
  * Particles
  */
 // Geometry
-const particlesCount = 200;
-const positions = new Float32Array(particlesCount * 3);
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
 
-for (let i = 0; i < particlesCount; i++) {
-  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
-  positions[i * 3 + 1] =
-    objectsDistance * 0.5 -
-    Math.random() * objectsDistance * sectionMeshes.length;
-  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+for(let i = 0; i < particlesCount; i++)
+{
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
 }
 
-const particlesGeometry = new THREE.BufferGeometry();
-particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
-);
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial({
-  color: "#e1e2ef",
-  sizeAttenuation: textureLoader,
-  size: 0.03,
-});
+    color: '#ffeded',
+    sizeAttenuation: textureLoader,
+    size: 0.03
+})
 
 // Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Group
-const cameraGroup = new THREE.Group();
-scene.add(cameraGroup);
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
 
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  35,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.z = 6;
-cameraGroup.add(camera);
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+cameraGroup.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  alpha: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Scroll
  */
-let scrollY = window.scrollY;
-let currentSection = 0;
+let scrollY = window.scrollY
+let currentSection = 0
 
-window.addEventListener("scroll", () => {
-  scrollY = window.scrollY;
-  const newSection = Math.round(scrollY / sizes.height);
-});
+
 
 /**
  * Cursor
  */
-const cursor = {};
-cursor.x = 0;
-cursor.y = 0;
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
 
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = event.clientY / sizes.height - 0.5;
-});
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+})
 
-/**
- * Disable Right Click
- */
-
-window.addEventListener(
-  "contextmenu",
-  function (e) {
-    // do something here...
-    e.preventDefault();
-  },
-  false
-);
 /**
  * Animate
  */
-const clock = new THREE.Clock();
-let previousTime = 0;
+const clock = new THREE.Clock()
+let previousTime = 0
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
-  // Animate camera
-  camera.position.y = (-scrollY / sizes.height) * objectsDistance;
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance
 
-  const parallaxX = cursor.x * 0.5;
-  const parallaxY = -cursor.y * 0.5;
-  cameraGroup.position.x +=
-    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
-  cameraGroup.position.y +=
-    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
+    const parallaxX = cursor.x * 0.5
+    const parallaxY = - cursor.y * 0.5
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
-  // Render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
